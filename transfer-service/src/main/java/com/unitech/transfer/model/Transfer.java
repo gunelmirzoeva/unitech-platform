ef031d6403c801551955dfe18d40259431843c57
j <eliyevrevan60@gmail.com>
2025-07-18 21:48:25 +0400

    Transfer

--- transfer-service/src/main/java/com/unitech/transfer/model/Transfer.java
+++ transfer-service/src/main/java/com/unitech/transfer/model/Transfer.java
@@ -2,6 +2,7 @@ package com.unitech.transfer.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "transfers")
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Transfer {
--- transfer-service/src/main/java/com/unitech/transfer/service/TransferService.java
+++ transfer-service/src/main/java/com/unitech/transfer/service/TransferService.java
@@ -4,8 +4,11 @@ import com.unitech.transfer.dto.TransferRequest;
 import com.unitech.transfer.event.TransferEventPublisher;
 import com.unitech.transfer.model.Transfer;
 import com.unitech.transfer.repository.TransferRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class TransferService {
@@ -19,5 +22,22 @@ public class TransferService {
     @Autowired
     private TransferEventPublisher eventPublisher;
 
-    public Transfer transfer(TransferRequest request){    }
+    @Transactional
+    public Transfer transfer(TransferRequest request){
+        var sender = accountClient.getAccountById(request.getSenderAccountId());
+        var receiver = accountClient.getAccountById(request.getReceiverAccountId());
+
+        if (sender.getBalance().compareTo(request.getAmount()) < 0){
+            throw new IllegalArgumentException("Sender does not have enough balance");
+        }
+
+        accountClient.decreaseBalance(sender.getId(), request.getAmount());
+        accountClient.increaseBalance(receiver.getId(), request.getAmount());
+
+        Transfer transfer = Transfer.builder().senderAccountId(request.getSenderAccountId()).recieverAcoountId(request.getReceiverAccountId()).amount(request.getAmount()).status("Success").timestamp(LocalDateTime.now()).build();
+
+        Transfer saved = transferRepository.save(transfer);
+        eventPublisher.publishTransferEvent(saved);
+        return saved;
+    }
 }
