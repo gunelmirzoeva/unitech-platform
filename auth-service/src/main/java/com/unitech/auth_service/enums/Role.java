8f67afae5ecfd81a36274294b5390752e43752b4
gunelmirzoeva <mirzoevagunel20@gmail.com>
2025-07-21 23:30:27 +0400

    auth service from beginning but still  needs some changes

---
+++ auth-service/src/main/java/com/unitech/auth_service/enums/Role.java
@@ -0,0 +1,5 @@
+package com.unitech.auth_service.enums;
+
+public enum Role {
+    USER, ADMIN
+}
--- auth-service/src/test/java/com/unitech/auth_service/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/unitech/auth_service/AuthServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.unitech.auth;
+package com.unitech.auth_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
